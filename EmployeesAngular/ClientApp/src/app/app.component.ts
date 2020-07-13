909012d34b4830364cdf2905d11353b19de26bb7
omunizb <oriolmuniz@gmail.com>
2020-07-13 13:58:16 +0200

    Angular GET functionality

--- EmployeesAngular/ClientApp/src/app/app.component.ts
+++ EmployeesAngular/ClientApp/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ClientApp';
+export class AppComponent implements OnInit { 
+  title = 'Employees'; 
+  currentEmployees: Employees[] = [];  
+  constructor(private http: HttpClient) { }  
+  
+  ngOnInit() {  
+    this.http.get<Employees[]>('https://localhost:44301/api/employees').subscribe(result => {  
+      this.currentEmployees = result;  
+    }, error => console.error(error));  
+  }  
+  
+}  
+  
+interface Employees {  
+  id: number;  
+  name: string;  
+  surname: string;  
+  job: string;  
+  salary: number;  
 }
--- EmployeesAngular/ClientApp/src/app/app.module.ts
+++ EmployeesAngular/ClientApp/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
