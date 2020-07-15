775b7ed1b4b69ac4ee07da6a3abe459a9cdadab7
omunizb <oriolmuniz@gmail.com>
2020-07-15 09:53:57 +0200

    completed HTTP PUT functionality

--- EmployeesAngular/ClientApp/src/app/app.component.ts
+++ EmployeesAngular/ClientApp/src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders, JsonpClientBackend } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
-import { summaryFileName } from '@angular/compiler/src/aot/util';
-import { NumberFormatStyle } from '@angular/common';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,6 +12,7 @@ export class AppComponent implements OnInit {
   baseUrl = 'https://localhost:44301/api/employees';
   currentEmployees: Employees[] = [];
   newEmployee: Employees;
+  employeeUrl: string;
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -41,6 +37,21 @@ export class AppComponent implements OnInit {
     this.http.post<Employees>(this.baseUrl, this.newEmployee, this.httpOptions).subscribe(newEmployee => {
       this.currentEmployees.push(newEmployee)
     });
+  }
+  
+  update(_id: number, _name: string, _surname: string, _job: string, _salary: number) {
+    this.newEmployee = {
+      id: +_id,
+      name: _name, 
+      surname: _surname, 
+      job: _job, 
+      salary: +_salary
+    }
+
+    this.employeeUrl = this.baseUrl + `/${_id}`;
+    this.currentEmployees[+_id - 1] = this.newEmployee;
+
+    this.http.put<Employees>(this.employeeUrl, this.newEmployee, this.httpOptions).subscribe();
   }  
 }
 
